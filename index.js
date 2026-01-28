b63be4a5e0bbe80f29233d8e192bfca1c659f432
Tsutame <chatrchaykhunwandi4@gmail.com>
2026-01-28 15:48:29 +0700

    V2

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
-const express=require('express');
+const express = require("express");
 const app = express();
-const port=3000;
+const port = 3008;
 
-app.get("/",(req,res)=>{
-     res.send("Mr.Puri Nangnoi");
-})
+app.use(express.json());
 
-app.listen(port,()=>{
-     console.log("Server Starting on port");
+let department = [
+  { id: 1, name: "IT" },
+  { id: 2, name: "DBT" },
+];
+
+app.get("/", (req, res) => {
+  res.send("Mr.Puri Nangnoi");
+});
+
+app.get("/department", (req, res) => {
+  res.send(department);
+});
+
+app.post("/department", (req, res) => {
+  let newdepartment = {
+    id: department.length + 1,
+    name: req.body.name,
+  };
+  department.push(newdepartment);
+  res.send(newdepartment);
+});
+
+app.listen(port, () => {
+  console.log("Server Starting on port");
 });
